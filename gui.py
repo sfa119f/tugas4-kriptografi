585e33fee3ca9b0583f5452cf57ce686974b6ba7
Syarifuddin <syarifuddinfa@gmail.com>
2021-11-03 22:40:40 +0700

    add downloadkey and fix bug

--- gui.py
+++ gui.py
@@ -1,3 +1,4 @@
+import json
 from tkinter import *
 import tkinter.messagebox
 from rsa import *
@@ -5,6 +6,8 @@ from paillier import *
 from elgamal import *
 
 def clearKeyBox():
+# Menutup semua input key
+  clearKey()
   keyLabel.place_forget()
   key.place_forget()
   gkeyLabel.place_forget()
@@ -56,12 +59,82 @@ def changeCipher():
 def clearKey():
 # Menghapus key yang ada di layar
   key.delete(0, 'end')
+  gkey.delete(0, 'end')
   pkey.delete(0, 'end')
   qkey.delete(0, 'end')
+  xkey.delete(0, 'end')
+  rkey.delete(0, 'end')
+  kkey.delete(0, 'end')
+
+def makeKeyFile(pbKey, pvKey):
+# Membuat file json key
+  res = { 'publicKey': pbKey, 'privateKey': pvKey }
+  jsonString = json.dumps(res)
+  jsonFile = open("key.json", "w")
+  jsonFile.write(jsonString)
+  jsonFile.close()
+  tkinter.messagebox.showinfo('Success', 'Key file downloaded successfully')
 
 def downloadKey():
 # Mendownload public key dan private key
-  print('downloadKey')
+  if cipher.get() == 'X':
+    tkinter.messagebox.showinfo('Error', 'Cipher type not selected')
+  elif cipher.get() == 'RSA':
+    try:
+      int(key.get())
+      int(pkey.get())
+      int(qkey.get())
+    except ValueError:
+      tkinter.messagebox.showinfo('Error', 'Key or P-key or Q-key only accept integer value')
+    else:
+      if not isRsaValidatePQ(int(pkey.get()), int(qkey.get())):
+        tkinter.messagebox.showinfo('Error', 'P-value or Q-value only accept prime value')
+      elif not isRsaValidateEkey(int(key.get()), int(pkey.get()), int(qkey.get())):
+        tkinter.messagebox.showinfo('Error', 'Key must be relatively prime with phi value')
+      else:
+        eKey, nValuePb = makePbKeyRsa(int(pkey.get()), int(qkey.get()), int(key.get()))
+        dKey, nValuePv = makePvKeyRsa(int(pkey.get()), int(qkey.get()), int(key.get()))
+        pbKey = { 'eKey': eKey, 'nValue': nValuePb }
+        pvKey = { 'dKey': dKey, 'nValue': nValuePv }
+        makeKeyFile(pbKey, pvKey)
+  elif cipher.get() == 'ElGamal':
+    try:
+      int(gkey.get())
+      int(pkey.get())
+      int(xkey.get())
+    except ValueError:
+      tkinter.messagebox.showinfo('Error', 'G-key or P-key or X-key only accept integer value')
+    else:
+      if not isElgamalValidatePGX(int(pkey.get()), int(gkey.get()), int(xkey.get())):
+        tkinter.messagebox.showinfo('Error', 'G-key or P-key or X-key is not acceptable'
+      else:
+        yVal, gVal, pValPb = makePublicKey(int(pkey.get()), int(gkey.get()), int(xkey.get()))
+        xVal, pValPv = makePrivateKey(int(xkey.get()), int(pkey.get()))
+        pbKey = { 'yValue': yVal, 'gValue': gVal, 'pValue': pValPb }
+        pvKey = { 'xValue': xVal, 'pValue': pValPv }
+        makeKeyFile(pbKey, pvKey)
+  elif cipher.get() == 'Paillier':
+    try:
+      int(gkey.get())
+      int(pkey.get())
+      int(qkey.get())
+    except ValueError:
+      tkinter.messagebox.showinfo('Error', 'G-key or P-key or Q-key only accept integer value')
+    else:
+      if not isPaillierValidatePQ(int(pkey.get()), int(qkey.get())):
+        tkinter.messagebox.showinfo('Error', 'P-value or Q-value must meet the requirements GCD(pq, (p-1)*(q-1)) = 1')
+      elif not isPaillierValidateG(int(gkey.get()), int(pkey.get()) * int(qkey.get())):
+        tkinter.messagebox.showinfo('Error', 'G-value must meet the requirements Key < n^2')
+      elif encrypt.get() and not isPaillierValidateR(int(rkey.get()), int(pkey.get()) * int(qkey.get())):
+        tkinter.messagebox.showinfo('Error', 'R-value must meet the requirement 0 <= r < n and GCD(r, n) = 1')
+      else:
+        gVal, nVal = makePbKeyPaillier(int(pkey.get()), int(qkey.get()), int(gkey.get()))
+        lamda, mu = makePvKeyPaillier(int(pkey.get()), int(qkey.get()), int(gkey.get()))
+        pbKey = { 'gValue': gVal, 'nValue': nVal }
+        pvKey = { 'lamda': lamda, 'mu': mu }
+        makeKeyFile(pbKey, pvKey)
+  elif cipher.get() == 'ECC':
+    tkinter.messagebox.showinfo('Error', 'ECC algorithm not implemented')
 
 def clearText():
 # Menghapus text yang ada di layar
@@ -113,10 +186,10 @@ def convertText():
         int(pkey.get())
         int(qkey.get())
       except ValueError:
-        tkinter.messagebox.showinfo('Error', 'Key or P-key or Q-key only accept integer value')
+        tkinter.messagebox.showinfo('Error', 'Key or P-value or Q-value only accept integer value')
       else:
         if not isRsaValidatePQ(int(pkey.get()), int(qkey.get())):
-          tkinter.messagebox.showinfo('Error', 'P-key or Q-key only accept prime value')
+          tkinter.messagebox.showinfo('Error', 'P-value or Q-value only accept prime value')
         elif not isRsaValidateEkey(int(key.get()), int(pkey.get()), int(qkey.get())):
           tkinter.messagebox.showinfo('Error', 'Key must be relatively prime with phi value')
         else:
@@ -161,14 +234,14 @@ def convertText():
         int(qkey.get())
         if encrypt.get(): int(rkey.get())
       except ValueError:
-        tkinter.messagebox.showinfo('Error', 'Key or P-key or Q-key or R-key only accept integer value')
+        tkinter.messagebox.showinfo('Error', 'G-value or P-value or Q-value or R-value only accept integer value')
       else:
         if not isPaillierValidatePQ(int(pkey.get()), int(qkey.get())):
-          tkinter.messagebox.showinfo('Error', 'P-key or Q-key must meet the requirements GCD(pq, (p-1)*(q-1)) = 1')
+          tkinter.messagebox.showinfo('Error', 'P-value or Q-value must meet the requirements GCD(pq, (p-1)*(q-1)) = 1')
         elif not isPaillierValidateG(int(gkey.get()), int(pkey.get()) * int(qkey.get())):
-          tkinter.messagebox.showinfo('Error', 'Key must meet the requirements Key < n^2')
+          tkinter.messagebox.showinfo('Error', 'G-value must meet the requirements Key < n^2')
         elif encrypt.get() and not isPaillierValidateR(int(rkey.get()), int(pkey.get()) * int(qkey.get())):
-          tkinter.messagebox.showinfo('Error', 'R-Key must meet the requirement 0 <= r < n and GCD(r, n) = 1')
+          tkinter.messagebox.showinfo('Error', 'R-value must meet the requirement 0 <= r < n and GCD(r, n) = 1')
         else:
           if encrypt.get():
             keyA, keyB = makePbKeyPaillier(int(pkey.get()), int(qkey.get()), int(gkey.get()))
@@ -178,7 +251,7 @@ def convertText():
           showOutput(output)
 
     elif cipher.get() == 'ECC':
-      ptkinter.messagebox.showinfo('Error', 'ECC algorithm not implemented')
+      tkinter.messagebox.showinfo('Error', 'ECC algorithm not implemented')
 
 root = Tk()
 root.title('Cipher')
@@ -218,7 +291,7 @@ kkeyLabel = Label(text='k-value')
 kkey = Entry(root, width=20)
 
 # Button delete key dan download key
-btnClearKey = Button(root, text='Clear Key', command=clearKey, bg='grey85')
+btnClearKey = Button(root, text='Clear Key', command=clearKey, bg='grey85', fg='red')
 btnClearKey.place(x=90, y=75)
 btnDownloadKey = Button(root, text='Download Key', command=downloadKey, bg='grey85')
 btnDownloadKey.place(x=160, y=75)
